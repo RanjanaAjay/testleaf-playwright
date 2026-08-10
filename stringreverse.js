177baafcac55df0fba4a780ea114c037bed32929
Ranjana <39521550+RanjanaAjay@users.noreply.github.com>
2026-08-10 19:02:16 +0530

    Add files via upload

---
+++ stringreverse.js
@@ -0,0 +1,6 @@
+let str="Testleaf"
+let rev=""
+for( let i=0;i<=str.length;i++){
+    rev=str.charAt(i)+rev
+}
+console.log(rev);
---
+++ testleafclass2.js
@@ -0,0 +1,13 @@
+
+ var firstName = 'Ranjana'    
+ var companyName = 'Test Leaf' 
+ var mobileNumber  = 90787787777   
+ var isAutomation
+ var hasPlaywright = true
+ console.log(typeof hasPlaywright) 
+ console.log('name is ', firstName ,'\n working at: ', companyName, '\n mobile number is ' , mobileNumber);
+ console.log(typeof firstName);
+ console.log(typeof companyName);
+ console.log(mobileNumber);
+ console.log(typeof isAutomation);
+ 
